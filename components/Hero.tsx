f6431d8b7738ef6785920d8bb1a667d8b0264d67
jihed5504 <jihed5504@gmail.com>
2024-11-17 18:20:46 +0100

    first update

---
+++ components/Hero.tsx
@@ -0,0 +1,84 @@
+import { FaLocationArrow, FaDownload } from "react-icons/fa"; // Import necessary icons
+import MagicButton from "./MagicButton";
+import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import Image from "next/image";
+import Link from "next/link";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36 relative">
+      <div className="absolute top-5 left-5 z-20">
+        <Image
+          src="/jsm-logo.png"
+          alt="Jihed Medini"
+          width={100}
+          height={100}
+          className="rounded-full shadow-lg"
+        />
+        {/* Updated button styles to match site design */}
+        <div className="mt-3">
+          <a
+            href="/Jihed_s_Resume_English.pdf"
+            download="Jihed_Medini_Resume.pdf"
+            className="flex items-center bg-indigo-700 hover:bg-indigo-800 text-white font-semibold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:-translate-y-1 shadow"
+            style={{
+              textDecoration: "none",
+              display: "inline-flex",
+              alignItems: "center",
+            }}
+          >
+            <FaDownload className="mr-2" /> {/* Icon with a margin-right */}
+            Resume
+          </a>
+        </div>
+      </div>
+
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="h-[80vh] w-[50vw] top-10 left-full"
+          fill="purple"
+        />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+
+      <div
+        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+       absolute top-0 left-0 flex items-center justify-center"
+      >
+        <div
+          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
+         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+        />
+      </div>
+
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            &#x2764; Keep it Simple &#x2764;
+          </p>
+          <TextGenerateEffect
+            words="Transforming Concepts into Seamless User Experiences"
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          />
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi! I’m Jihed, a Full-Stack Web Developer based in Tunisia.
+          </p>
+          {/* <Link href="#about" passHref>
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </Link> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ components/ui/FloatingNavbar.tsx
@@ -0,0 +1,85 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { motion, AnimatePresence, useScroll } from "framer-motion";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { debounce } from "lodash";
+
+export const FloatingNav = ({
+  navItems,
+  className,
+}: {
+  navItems: {
+    name: string;
+    link: string;
+    icon?: JSX.Element;
+  }[];
+  className?: string;
+}) => {
+  const { scrollYProgress } = useScroll();
+  const [visible, setVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.pageYOffset;
+      const direction = currentScrollY - lastScrollY;
+      setVisible(currentScrollY < 50 || direction < 0);
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY]);
+
+  return (
+    <AnimatePresence>
+      {visible && (
+        <motion.div
+          initial={{ opacity: 0, y: -10 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -10 }}
+          transition={{ duration: 0.2 }}
+          className={cn(
+            "flex max-w-fit md:min-w-[70vw] lg:min-w-fit fixed z-[5000] top-10 inset-x-0 mx-auto px-10 py-5 rounded-lg border border-black/.1 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] items-center justify-center space-x-4",
+            className
+          )}
+          style={{
+            backdropFilter: "blur(16px) saturate(180%)",
+            backgroundColor: "rgba(17, 25, 40, 0.75)",
+            borderRadius: "12px",
+            border: "1px solid rgba(255, 255, 255, 0.125)",
+          }}
+        >
+          {navItems.map((navItem, idx) => (
+            <Link
+              key={`nav-item-${idx}`}
+              href={navItem.link}
+              scroll={false} // Prevent Next.js's default scroll behavior
+              onClick={(e) => {
+                e.preventDefault(); // Prevent default anchor jump
+                const targetElement = document.querySelector(navItem.link);
+                if (targetElement) {
+                  const offset =
+                    document.querySelector(".navbar")?.offsetHeight || 0;
+                  const top =
+                    targetElement.getBoundingClientRect().top +
+                    window.pageYOffset -
+                    offset;
+
+                  window.scrollTo({ top, behavior: "smooth" });
+                }
+              }}
+              className="relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500 !cursor-pointer"
+            >
+              {navItem.icon && (
+                <span className="block sm:hidden">{navItem.icon}</span>
+              )}
+              <span className="text-sm">{navItem.name}</span>
+            </Link>
+          ))}
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
